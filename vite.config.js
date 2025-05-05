b0d4b24d4217bfcb0fb301b16747dbb4d0a404c6
mostafa <mostafaghr12345@gmail.com>
2025-05-05 16:02:09 +0330

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/React-Simple-Auth-based-Data-Viewer/",
+  base: "./",
   plugins: [react(),
   tailwindcss(),
   ],
